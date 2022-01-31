879d7fd86c693dc454229842906e6b7073cc71dd
Kushagra20075 <Kushagra20075@iiitd.ac.in>
2022-02-01 00:39:58 +0530

    Some Work Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {useEffect} from "react";
+import "./App.css";
+import { createStore } from 'redux';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import Header from "./Components/Header";
+import { auth } from "./States/firebase";
+import { useStateValue } from "../States/StateProvider";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  useEffect(() => {
+    // will only run once when the app component loads...
 
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+return (
+  <div className="app">
+    <Header/>
+    <Router>
+      <Routes>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Routes>
+    </Router>
+  </div>
+);
+}
 export default App;
---
+++ src/States/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCEqGPyOiQciuyDB1tL87WbcaNBqClC2R8",
+    authDomain: "envagle-beauty.firebaseapp.com",
+    projectId: "envagle-beauty",
+    storageBucket: "envagle-beauty.appspot.com",
+    messagingSenderId: "352592925650",
+    appId: "1:352592925650:web:32b01b1c8464df04ca6e0b",
+    measurementId: "G-9Q58HM1FSY"
+  };
+
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+const db = getFirestore();
+
+
+const auth = getAuth();
+
+export { db, auth };
